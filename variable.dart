f51c5a32aaafa850153b54289a35cc3cf026538b
joseph Munyao <josephmunyao019@gmail.com>
2024-03-06 04:13:22 -0800

    commit changes

---
+++ variable.dart
@@ -0,0 +1,15 @@
+void main() {
+  //variables in dart
+  String school = 'PLP AFRICA ACADEMY';
+  int total = 400;
+  double GPA = 4.0;
+  num year = 2024;
+  bool ismarried = true;
+
+  //print our variables
+  print(school);
+  print(total);
+  print(GPA);
+  print(year);
+  print(ismarried);
+}
