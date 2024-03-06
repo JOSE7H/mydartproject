3a0bbfe648482fa7ba853b63371e9037b8e866c1
joseph Munyao <josephmunyao019@gmail.com>
2024-03-06 05:04:14 -0800

    commit

---
+++ data_types/numbers.dart
@@ -0,0 +1,18 @@
+void main() {
+  //Declearing variables
+  int num1 = 100; //without decimal point
+  double num2 = 156.5; //with decimal point
+  num num3 = 50;
+  num num4 = 30.7;
+  //for sum
+  num sum = num1 + num2 + num3 + num4;
+
+  //printing information
+  print("num 1 is $num1");
+  print("num 2 is $num2");
+  print("num 3 is $num3");
+  print("num 4 is $num4");
+  print("sum is $sum");
+
+  //for subtraction
+}
---
+++ data_types/strings.dart
@@ -0,0 +1,10 @@
+void main() {
+  //declearing values
+  String schoolname = "power Project Academy";
+  String address = "Africa";
+  String module = 'Dart';
+
+  //printing values
+  print(
+      "My School name is $schoolname and the address is $address. The class in progress is $module");
+}
