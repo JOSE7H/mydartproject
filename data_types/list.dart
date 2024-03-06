dd8be63b3622a947d613735b9a989167c7eb7ff2
joseph Munyao <josephmunyao019@gmail.com>
2024-03-06 05:18:09 -0800

    commit changes

---
+++ data_types/list.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> names = ["mutua", "John", "Mercy"];
+  print("value of the name is $names");
+  print("value of names[0] is ${names[0]}"); //index 0
+  print("value of names[1] is ${names[1]}"); //index 1
+  print("value of names[2] is ${names[2]}"); //index 2
+
+  print(names);
+}
